efd8cb98c82d073fb8a6afd21c3c9f1124a6bd12
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2023-10-20 22:04:13 +0300

    commit_2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -17,11 +17,13 @@ export const ContactList =({contacts,onDeleteContact})=>{
  )
 }
 
-ContactList.propTypes={
-
-   contacts:PropTypes.arrayOf( PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,}).isRequired),
-      onDeleteContact: PropTypes.func.isRequired,
-}
+ContactList.propTypes = {
+   contacts: PropTypes.arrayOf(
+     PropTypes.shape({
+       id: PropTypes.string.isRequired,
+       name: PropTypes.string.isRequired,
+       number: PropTypes.string.isRequired,
+     }),
+   ),
+   onDeleteContact: PropTypes.func.isRequired,
+ };
